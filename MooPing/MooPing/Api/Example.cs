55fea9ed96026dea74dbab7c144804a5b79a4a14
Nez <checkman3335@gmail.com>
2024-02-29 19:55:25 -0800

    Simple Carter Api example

---
+++ MooPing/MooPing/Api/Example.cs
@@ -0,0 +1,24 @@
+﻿using Carter;
+
+namespace MooPing.Api
+{
+    public class ExampleModule : CarterModule
+    {
+        private readonly ILogger<ExampleModule> _logger;
+        public ExampleModule(ILogger<ExampleModule> logger) : base("/api/example")
+        {
+            base.WithTags("Examples");
+            this._logger = logger;
+        }
+        public override void AddRoutes(IEndpointRouteBuilder app)
+        {
+            //Get Request
+            app.MapGet("/", () =>
+            {
+                return Results.Ok("Ok");
+            });
+
+            app.MapGet("/hello", () => "Hello World");
+        }
+    }
+}
