5bcc7ffe7d29ae1707ad4921752475861f9bbc6a
Nez <checkman3335@gmail.com>
2024-02-29 19:41:47 -0800

    Add logging and some services

--- MooPing/MooPing/Program.cs
+++ MooPing/MooPing/Program.cs
@@ -1,11 +1,70 @@
+using Carter;
 using MooPing.Components;
+using NpgsqlTypes;
+using Serilog.Sinks.PostgreSQL.ColumnWriters;
+using Serilog.Sinks.PostgreSQL;
+using Serilog;
+using Serilog.Events;
+using Serilog.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
+#region Services
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCarter();
+
+builder.Services.AddWebOptimizer(pipeline =>
+{
+    pipeline.AddCssBundle("/css/site.css", "scss/site.scss");
+    pipeline.MinifyJsFiles("js/**/*.js", "js/**/*.js");
+});
+#endregion
+
+#region Logging
+
+// Configure serilog logging
+//Connection string is from Secret Manager. (Right-click on project and select "Manage User Secrets")
+var logConnectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
+
+if (!string.IsNullOrEmpty(logConnectionString))
+{
+    IDictionary<string, ColumnWriterBase> columnOptions = new Dictionary<string, ColumnWriterBase>
+{
+    { "message", new RenderedMessageColumnWriter(NpgsqlDbType.Text) },
+    { "message_template", new MessageTemplateColumnWriter(NpgsqlDbType.Text) },
+    { "level", new LevelColumnWriter(true, NpgsqlDbType.Varchar) },
+    { "raise_date", new TimestampColumnWriter(NpgsqlDbType.TimestampTz) },
+    { "exception", new ExceptionColumnWriter(NpgsqlDbType.Text) },
+    { "properties", new LogEventSerializedColumnWriter(NpgsqlDbType.Jsonb) },
+    { "props_test", new PropertiesColumnWriter(NpgsqlDbType.Jsonb) },
+    { "machine_name", new SinglePropertyColumnWriter("MachineName", PropertyWriteMethod.ToString, NpgsqlDbType.Text, "l") }
+};
+
+    var loggerConfiguration = new LoggerConfiguration().Filter.ByExcluding(le => Matching.FromSource("Microsoft").Invoke(le)
+                         && (le.Level == LogEventLevel.Verbose
+                         || le.Level == LogEventLevel.Debug
+                         || le.Level == LogEventLevel.Information))
+                        .WriteTo.PostgreSQL(
+                                    connectionString: logConnectionString,
+                                    columnOptions: columnOptions,
+                                    needAutoCreateTable: true,
+                                    schemaName: "log",
+                                    tableName: "Serilog"
+                                    ).Enrich.FromLogContext();
+
+    var logger = loggerConfiguration.CreateLogger();
+    builder.Services.AddLogging(loggingBuilder =>
+    {
+        loggingBuilder.AddSerilog(logger);
+    });
+}
+#endregion
 
 var app = builder.Build();
 
